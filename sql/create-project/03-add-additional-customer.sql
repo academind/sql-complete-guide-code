ef8a7941f5419d5d4b83a59f1a68366d4afc3735
Manuel Lorenz <manuellorenz@Manuels-Mini.fritz.box>
2021-11-16 15:33:38 +0100

    added additional customer

---
+++ sql/create-project/03-add-additional-customer.sql
@@ -0,0 +1,18 @@
+INSERT INTO customers(
+    first_name,
+    last_name,
+    email
+)
+VALUES(
+    'Maria',
+    'Jones',
+    'maria@test.com'
+);
+
+INSERT INTO orders(
+   amount_billed,
+   customer_id 
+)
+VALUES(
+    103.12
+);
