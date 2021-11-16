52fd7518c1bcf77a375fc52fe06057c1201759f9
Manuel Lorenz <manuellorenz@Manuels-Mini.fritz.box>
2021-11-16 15:14:52 +0100

    added additional member

---
+++ sql/create-project/03.01-add-additional-user.sql
@@ -0,0 +1,25 @@
+INSERT INTO memberships (
+    membership_start,
+    membership_end,
+    last_checkin,
+    last_checkout,
+    consumption,
+    first_name,
+    last_name,
+    price,
+    billing_frequency,
+    gender
+
+)
+VALUES (
+    '2021-05-02',
+    NULL,
+    '2021-06-05 11:52:25',
+    '2021-06-05 11:58:08',
+    NULL,
+    'Monti',
+    'Durns',
+    199.99,
+    1,
+    'male   '
+);
